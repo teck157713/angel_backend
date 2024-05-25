8a089caf64036a3adda06d67f80a7f4e3800c1b8
teck157713 <teck.157713@hotmail.com>
2024-05-26 02:57:08 +0800

    Added a helper function that will compute the transaction amounts.

---
+++ src/utils/payment.utils.ts
@@ -0,0 +1,41 @@
+import { Timestamp } from "firebase-admin/firestore";
+import { firestore } from "../firebase";
+
+export const getTransactionInformation = async (uid: string, originalAmount: number) => {
+    // Get the user's donation limit
+    const userPreferenceSnapshot = await firestore
+        .doc(`/users/${uid}`)
+        .get();
+
+    const limit = userPreferenceSnapshot.data()?.donationlimit;
+
+    // Get the total donated amount this month
+    const now = new Date();
+    const transactionSnapshots = await firestore
+        .collection(`/users/${uid}/transactions`)
+        .where("date", ">", Timestamp.fromDate(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0)))
+        .get();
+
+    const totalDonatedAmount = transactionSnapshots.docs.reduce((acc, curr) => acc += curr.data()?.donatedAmount || 0, 0);
+
+    // Do not do donations if it exceeds the limmit!
+    if (limit && totalDonatedAmount >= limit) {
+        return {
+            totalAmount: originalAmount,
+            donatedAmount: 0,
+            originalAmount,
+            taxDeducted: 0
+        }
+    }
+
+    const totalAmount = Math.ceil(originalAmount);
+    const donatedAmount = originalAmount - totalAmount;
+    const taxDeducted = donatedAmount * 2.5;
+
+    return {
+        totalAmount,
+        donatedAmount,
+        originalAmount,
+        taxDeducted
+    }
+}
